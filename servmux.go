93ae7a4b063a4d45cf22710dd805b44c4fc61384
Dustin Sallings <dustin@spy.net>
2013-09-03 11:33:32 -0700

    A dispatch mux

---
+++ servmux.go
@@ -0,0 +1,93 @@
+package coap
+
+import (
+	"net"
+	"path"
+)
+
+type ServeMux struct {
+	m map[string]muxEntry
+}
+
+type muxEntry struct {
+	h       RequestHandler
+	pattern string
+}
+
+func NewServeMux() *ServeMux { return &ServeMux{m: make(map[string]muxEntry)} }
+
+// Does path match pattern?
+func pathMatch(pattern, path string) bool {
+	if len(pattern) == 0 {
+		// should not happen
+		return false
+	}
+	n := len(pattern)
+	if pattern[n-1] != '/' {
+		return pattern == path
+	}
+	return len(path) >= n && path[0:n] == pattern
+}
+
+// Return the canonical path for p, eliminating . and .. elements.
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+	return np
+}
+
+// Find a handler on a handler map given a path string
+// Most-specific (longest) pattern wins
+func (mux *ServeMux) match(path string) (h RequestHandler, pattern string) {
+	var n = 0
+	for k, v := range mux.m {
+		if !pathMatch(k, path) {
+			continue
+		}
+		if h == nil || len(k) > n {
+			n = len(k)
+			h = v.h
+			pattern = v.pattern
+		}
+	}
+	return
+}
+
+func notFoundHandler(l *net.UDPConn, a *net.UDPAddr, m *Message) *Message {
+	return &Message{
+		Type: Acknowledgement,
+		Code: NotFound,
+	}
+}
+
+var _ = RequestHandler(&ServeMux{})
+
+func (mux *ServeMux) Handle(l *net.UDPConn, a *net.UDPAddr, m *Message) *Message {
+	h, _ := mux.match(m.PathString())
+	if h == nil {
+		h, _ = funcHandler(notFoundHandler), ""
+	}
+	// TODO:  Rewrite path?
+	return h.Handle(l, a, m)
+}
+
+func (mux *ServeMux) Register(pattern string, handler RequestHandler) {
+	if pattern == "" {
+		panic("http: invalid pattern " + pattern)
+	}
+	if handler == nil {
+		panic("http: nil handler")
+	}
+
+	mux.m[pattern] = muxEntry{h: handler, pattern: pattern}
+}
